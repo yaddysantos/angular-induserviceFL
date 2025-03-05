a57c46305e3a9dbc6b4feb0264ee7c8134419b72
yaddysantos <34655963+yaddysantos@users.noreply.github.com>
2025-03-05 12:25:07 -0500

    update footer structure

--- src/lib/servicios.model.ts
+++ src/lib/servicios.model.ts
@@ -1,15 +1,16 @@
 export enum ServicesList {
-    LATONERIA = 'Latonería',
-    PORCELANIZADA = 'Porcelanizada',
-    FIBRA = 'Fibra de vidrio',
-    PINTURA = 'Pintura automotriz',
-    PLASTICO = 'Reparación de plásticos',
- }
+  PORCELANIZADA = 'Porcelanizado',
+  FIBRA = 'Fibra de vidrio',
+  LATONERIA = 'Latonería especializada',
+  PINTURA = 'Restauracion de pintura automotriz',
+  PLASTICO = 'Reparación de todo tipo plásticos',
+  SOLDADURA = 'Soldadura General',
+}
 
- export interface Photo {
-    title: string,
-    etapa1: string,
-    etapa2: string,
-    etapa3: string,
-    etapa4: string,
- }
+export interface Photo {
+  title: string;
+  etapa1: string;
+  etapa2: string;
+  etapa3: string;
+  etapa4: string;
+}
